bdd64204ddda00af2513a12d913371d14aab2f9d
Boris Kayi <hello@silverhairs.engineer>
2023-08-03 22:59:12 +0200

    fix(tree.py): --json flag does not parse the ast before encoding

--- tree.py
+++ tree.py
@@ -9,16 +9,30 @@ def get_ast(file_path: str, as_json=False):
     with open(file_path, 'r') as file:
         source = file.read()
     tree = ast.parse(source)
-    out = ast.dump(tree)
-
     if as_json:
-        out = json.dumps(out, indent=2)
-    return out
+        dict_tree = ast_to_dict(tree)
+        return json.dumps(dict_tree, indent=2)
+
+    return ast.dump(tree)
+
+
+def ast_to_dict(node):
+    if isinstance(node, ast.AST):
+        fields = {}
+        for field, value in ast.iter_fields(node):
+            fields[field] = ast_to_dict(value)
+        return {
+            "ast_type": type(node).__name__,
+            "fields": fields
+        }
+    elif isinstance(node, list):
+        return [ast_to_dict(item) for item in node]
+    return node
 
 
 
 if __name__ == '__main__':
-    if not len(sys.argv) > 2:
+    if not len(sys.argv) >= 2:
         print('Wrong number of argument.\nUsage: python tree.py <file_path> [--json]')
         sys.exit(1)
 
